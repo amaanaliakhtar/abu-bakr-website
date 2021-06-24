67d89aa075c7a1c1e26f4838798b2219326c34be
Amaan <amaanaliakhtar@gmail.com>
2021-06-24 15:20:09 +0100

    add hero section video

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
 
 function App() {
 	return (
@@ -9,7 +10,7 @@ function App() {
 			<Router>
 				<Navbar></Navbar>
 				<Switch>
-					<Route path="/" exact></Route>
+					<Route path="/" exact component={Home}></Route>
 				</Switch>
 			</Router>
 		</>
