7667df00d1cff69bed6e858c665a0b504b5f74a7
Amaan <amaanaliakhtar@gmail.com>
2021-06-27 13:03:42 +0100

    added cards, need edit

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function CardItem() {
+function CardItem(props) {
 	return (
 		<>
 			<li className="cards-item">
-				<Link className="cards-item-link">
-					<figure className="cards-item-pic-wrap">
-						<img src="/" alt="Card" className="cards-item-img"></img>
+				<Link className="cards-item-link" to={props.path}>
+					<figure className="cards-item-pic-wrap" data-category={props.label}>
+						<img src={props.src} alt="Card" className="cards-item-img"></img>
 					</figure>
 					<div className="cards-item-info">
-						<h5 className="cards-item-text">lorem ipsum</h5>
+						<h5 className="cards-item-text">{props.text}</h5>
 					</div>
 				</Link>
 			</li>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,11 +5,17 @@ import "./Cards.css";
 function Cards() {
 	return (
 		<div className="cards">
-			<h1>Check this out</h1>
+			<h1>Check out these epic destinations</h1>
 			<div className="cards-container">
 				<div className="cards-wrapper">
 					<ul className="cards-items">
-						<CardItem></CardItem>
+						<CardItem src="images/img-9.jpg" text="Explore what we provide" label="Services" path="/services"></CardItem>
+						<CardItem src="images/img-2.jpg" text="Donate to the project" label="Donate" path="/project"></CardItem>
+					</ul>
+					<ul className="cards-items">
+						<CardItem src="images/img-3.jpg" text="Explore what we provide" label="Services" path="/services"></CardItem>
+						<CardItem src="images/img-4.jpg" text="Donate to the project" label="Donate" path="/project"></CardItem>
+						<CardItem src="images/img-8.jpg" text="Donate to the project" label="Donate" path="/project"></CardItem>
 					</ul>
 				</div>
 			</div>
