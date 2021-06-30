fdcdb1c9cc24b1a3e27fa02dcab0917cc928fb5c
Amaan <amaanaliakhtar@gmail.com>
2021-06-30 15:28:49 +0100

    change card images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,17 +5,27 @@ import CardItem from "./CardItem";
 function Cards() {
 	return (
 		<div className="cards">
-			<h1>Check out these EPIC Destinations!</h1>
+			<h1>Abu Bakr Masjid</h1>
 			<div className="cards-container">
 				<div className="cards-wrapper">
 					<ul className="cards-items">
-						<CardItem src="images/img-9.jpg" text="Explore the hidden waterfall deep inside the Amazon Jungle" label="Adventure" path="/services" />
-						<CardItem src="images/img-2.jpg" text="Travel through the Islands of Bali in a Private Cruise" label="Luxury" path="/project" />
+						<CardItem
+							src="images/quran-window.jpg"
+							text="Explore the hidden waterfall deep inside the Amazon Jungle"
+							label="Adventure"
+							path="/services"
+						/>
+						<CardItem src="images/masjid-night.jpg" text="Travel through the Islands of Bali in a Private Cruise" label="Luxury" path="/project" />
 					</ul>
 					<ul className="cards-items">
-						<CardItem src="images/img-3.jpg" text="Set Sail in the Atlantic Ocean visiting Uncharted Waters" label="Mystery" path="/services" />
-						<CardItem src="images/img-4.jpg" text="Experience Football on Top of the Himilayan Mountains" label="Adventure" path="/products" />
-						<CardItem src="images/img-8.jpg" text="Ride through the Sahara Desert on a guided camel tour" label="Adrenaline" path="/donate" />
+						<CardItem
+							src="images/street-prayer.jpg"
+							text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+							label="Mystery"
+							path="/services"
+						/>
+						<CardItem src="images/quran-dates.jpg" text="Experience Football on Top of the Himilayan Mountains" label="Adventure" path="/project" />
+						<CardItem src="images/charity.jpg" text="Ride through the Sahara Desert on a guided camel tour" label="Adrenaline" path="/donate" />
 					</ul>
 				</div>
 			</div>
